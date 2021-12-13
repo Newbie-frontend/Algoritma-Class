1176452f5a96a11a4baec3c5f895d8fa1f9ef3bb
hossein <hos.raz1993@gmail.com>
2021-12-13 13:49:24 +0300

    Algoritma class

---
+++ HR.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <>
+
+using namespace std;
+int main(){
+    int ap[n][n]= {{10,-25, 40}, {12 ,55 , 90}, 68 , 40 , 37}};
+    cout << 
+    for (int k = 0; k < n; k++)
+    {
+        for (int  j = 0; j < n; j++)
+        {
+            count << a[n][n]<< "\t";
+
+        }
+        count << "\n";
+    }
+    getch();
+}
